00f2565aeda518452daf0900cc1fd35486e35d32
Baitala <pedro.morshun@gmail.com>
2019-09-01 12:45:46 +0300

    Ex28

---
+++ Sep_HW/ex28.py
@@ -0,0 +1,17 @@
+
+
+def max_of_three(a, b=4, c=3):
+    """
+    Function takes three arugments and returns the biggest one
+    """
+    max = a
+    for number in b, c:
+        if number > max:
+            max = number
+    return max
+
+def any_function():
+    """
+    Dummy fuction
+    """
+    pass
---
+++ Sep_HW/main _ex28.py
@@ -0,0 +1,9 @@
+from ex28 import max_of_three
+
+x = input("Print the fisrth number ")
+y = input("Print the second number ")
+z = input("Print the third number ")
+
+print(max_of_three(x,y,z))
+print(max_of_three(100, c=1000))
+
