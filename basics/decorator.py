2bf4987082ff0181e45d036c1cd8f58f09eaa25a
Eugene Uvin <nivus.ua@gmail.com>
2019-09-09 00:27:12 +0300

    File with decorators syntax examples.

---
+++ basics/decorator.py
@@ -0,0 +1,29 @@
+
+def my_decorator(somefunc):
+    def decorating_func(arg):
+        print("Top text")
+        somefunc(arg)
+        print("Bottom text")
+        somefunc(arg)
+    return decorating_func
+
+@my_decorator
+def myfunc(line):
+    print(line)
+
+def html_decorator(infunc):
+    def html_wrapper(arg):
+        print("<html><head/><body><p>")
+        infunc(arg)
+        print("</p></body></html>")
+    return html_wrapper
+
+@html_decorator
+def just_printing(text):
+    print(text)
+
+myfunc("sometext")
+
+print("\n ================================= \n")
+
+just_printing("Lorem ipsem dolor sit amet...")
