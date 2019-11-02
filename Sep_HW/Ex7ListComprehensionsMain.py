7efa497967d163b295fef7d53ebcea238d7844c3
Baitala <pedro.morshun@gmail.com>
2019-11-02 14:44:07 +0200

    Ex7 implementation

--- Sep_HW/Ex7ListComprehensionsMain.py
+++ Sep_HW/Ex7ListComprehensionsMain.py
@@ -2,8 +2,27 @@
 # , 64, 81, 100]. Write one line of Python that takes this list a and makes a 
 # new list that has only the even elements of this list in it.
 
-a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-l = [for i in a]
+import random
+
+numlist = []
+list_length = random.randint(5,15)
+
+
+while len(numlist) < list_length:
+    numlist.append(random.randint(1,75))
+    
+
+evenlist = [number for number in numlist if number % 2 == 0] 
+
+print(numlist)
+print(evenlist)
+
+
+
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# b = [number for number in a if number % 2 == 0]
+
+# print(b)
 
 
 # Compare to this piece of code:
