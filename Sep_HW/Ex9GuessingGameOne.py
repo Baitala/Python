0dcc8e9aa538cddee11fec0a038f4856f5563e3c
Baitala <pedro.morshun@gmail.com>
2019-11-12 23:03:00 +0200

    Ex9 sketch

---
+++ Sep_HW/Ex9GuessingGameOne.py
@@ -0,0 +1,34 @@
+# Generate a random number between 1 and 9 (including 1 and 9). Ask the user to
+#  guess the number, then tell them whether they guessed too low, too high, or 
+# exactly right. (Hint: remember to use the user input lessons from the very 
+# first exercise)
+
+# Extras:
+
+#    Keep the game going until the user types “exit”
+#    Keep track of how many guesses the user has taken, and when the game ends,
+#  print this out.
+
+
+import random
+
+
+def guessing_game(guess_number):
+secret_number = random.randint(1, 9)
+
+if user_number == "exit":
+    quit
+elif secret_number == 
+
+
+
+
+
+
+    OneMoreTime = input("""Do you want to play one more time? y/n
+    """)
+print("You have guessed " +  + " times!!!")
+
+if __name__ == "__main__":
+    # print ("Pass 1" if guessing_game("exit") else "Error1")
+    print("Error2" if guessing_game(secret_number) != )
