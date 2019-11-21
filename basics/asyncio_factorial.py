ad26609b5c08f5a08682edf5431e7a47de17f3ba
Eugene Uvin <nivus.ua@gmail.com>
2019-11-21 13:01:28 -0500

    Compatible with Python 3.6

--- basics/asyncio_factorial.py
+++ basics/asyncio_factorial.py
@@ -1,12 +1,17 @@
+# Test for async/await from official doc
+# https://docs.python.org/3.6/library/asyncio-task.html
+# All commented lines are for Python 3.7
 import asyncio
 
 async def factorial(name, number):
     f = 1
     for i in range(2, number + 1):
-        print("Task {name}: Compute factorial({i})...")
+        #print(f"Task {name}: Compute factorial({i})...")
+        print("Task %s: Compute factorial(%s)..." % (name, i))
         await asyncio.sleep(1)
         f *= i
-    print("Task {name}: factorial({number}) = {f}")
+    #print(f"Task {name}: factorial({number}) = {f}")
+    print("Task %s: factorial(%s) = %s" % (name, number, f))
 
 async def main():
     # Schedule three calls *concurrently*:
@@ -16,4 +21,11 @@ async def main():
         factorial("C", 4),
     )
 
-asyncio.run(main())
+#asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.gather(
+    factorial("A", 2),
+    factorial("B", 3),
+    factorial("C", 4),
+))
+loop.close()
