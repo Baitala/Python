343f1c0601c7a901ae4e78fb3ed5ca51206464c5
Baitala <pedro.morshun@gmail.com>
2019-12-06 18:23:40 +0200

    Ex15 Sketch

---
+++ Sep_HW/Ex15ReverseWordOrder.py
@@ -0,0 +1,21 @@
+# Write a program (using functions!) that asks the user for a long string 
+# containing multiple words. Print back to the user the same string, except 
+# with the words in backwards order. For example, say I type the string:
+
+#   My name is Michele
+
+# Then I would see the string:
+
+#   Michele is name My
+
+# shown back to me.
+
+def reverse_string(user_string):
+    pass
+
+
+
+if __name__ == "__main__":
+    a = "My name is Michele"
+    # b = "Michele is name My"
+    print("Pass1" if reverse_string(a) == "Michele is name My" else "Error1")
