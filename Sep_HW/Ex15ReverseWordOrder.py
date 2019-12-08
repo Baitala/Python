a1f85d9cda986028a67c767c642152182c2dbf2f
Baitala <pedro.morshun@gmail.com>
2019-12-08 17:22:09 +0200

    Ex 15 kind of final implementation

--- Sep_HW/Ex15ReverseWordOrder.py
+++ Sep_HW/Ex15ReverseWordOrder.py
@@ -1,25 +1,27 @@
 # Write a program (using functions!) that asks the user for a long string 
 # containing multiple words. Print back to the user the same string, except 
 # with the words in backwards order. For example, say I type the string:
-
 #   My name is Michele
-
 # Then I would see the string:
-
 #   Michele is name My
-
 # shown back to me.
 
 def reverse_string(user_string):
     user_list = user_string.split(" ")
     length = len(user_list)
-    return length
+    reverse_list = []
+    iterator = length -1
+    while iterator >= 0:
+        reverse_list.append(user_list[iterator])
+        iterator -= 1
+    rev_string = " ".join(reverse_list)
+    return rev_string
 
 
 a = "My name is Michele"
 print(reverse_string(a))
 
-# if __name__ == "__main__":
-#     a = "My name is Michele"
-#     # b = "Michele is name My"
-#     print("Pass1" if reverse_string(a) == "Michele is name My" else "Error1")
+if __name__ == "__main__":
+    a = "My name is Michele"
+    print("Pass1" if reverse_string(a) == "Michele is name My" else "Error1")
+    print("Pass2" if reverse_string("Lazy fox is jumping over a") == "a over jumping is fox Lazy" else "Error2")
