db8aa98bc817b550e0e14dfdbe7e567f5c4f940c
Baitala <pedro.morshun@gmail.com>
2019-12-22 17:51:28 +0200

    additional

--- Sep_HW/Lecture_10.py
+++ Sep_HW/Lecture_10.py
@@ -8,9 +8,9 @@ def fib_rec(n):
 print(fib_rec(30))
 
 def fib_dinamic():
-fib = [0, 1] + [0] * (n-1)
-for i in range(2, n+1):
-    fib[i] = fib[i-1] + fib[i-2]
+    fib = [0, 1] + [0] * (n-1)
+    for i in range(2, n+1):
+        fib[i] = fib[i-1] + fib[i-2]
     return fib[n]
 
 #set of algorithm of dinamic programming
--- Sep_HW/Lecture_11.py
+++ Sep_HW/Lecture_11.py
@@ -13,7 +13,7 @@ def lcs(A, B):
 
 def gis(A):
     F = [0]*(len(A)+1)
-    for in in range(1, len(A)+1):
+    for in range(1, len(A)+1):
         m = 0
         for j in range (0, i):
             if A[i] > A[j] and F[j] > m:
