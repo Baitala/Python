0d1ba01dd58dd46f0ded4c897193da90a2d950bc
Baitala <pedro.morshun@gmail.com>
2019-12-22 17:50:53 +0200

    Lecture 11

---
+++ Sep_HW/Lecture_11.py
@@ -0,0 +1,22 @@
+
+
+def lcs(A, B):
+    F = [[0]* (len(B)+1) for i in range (len(A)+1)]
+    for i in range (1, len(A)+1):
+        for j in range (1, len(B)+1):
+            if A[i-1] == B[j-1]:
+                F[i][j] == 1+F[i-1][j-1]
+            else:
+                F[i][j] = max(F[i-1][j], F[i][j-1])
+    return F[-1][-1]
+
+
+def gis(A):
+    F = [0]*(len(A)+1)
+    for in in range(1, len(A)+1):
+        m = 0
+        for j in range (0, i):
+            if A[i] > A[j] and F[j] > m:
+                m = F[j]
+        F[i] = m+1
+    return F[len(A)]
