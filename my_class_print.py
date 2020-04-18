eb1ee9fc51a49d4cef0e936eaa9f801097eff2d1
pedro <pedro@pedro.perot>
2020-04-18 13:48:30 +0300

    Some changes

---
+++ my_class_print.py
@@ -0,0 +1,13 @@
+import pupil_lib
+
+p1 = pupil_lib.Pupil("Eugene",39,[1,2,3,4,5])
+p2 = pupil_lib.Pupil("Taras",14,[2,3,6])
+p3 = pupil_lib.Pupil("Ivan",19,[1,2,3])
+
+print(p1)
+print(p2)
+print(p3)
+
+jskdfl = p1.average_mark()
+
+print(jskdfl)
--- pupil_lib.py
+++ pupil_lib.py
@@ -1,11 +1,11 @@
 # define pupil class with it's properties (avarage mark, marks, home work (done, not done), e. t. c.).
 #  Spread the idea
-class pupil:
+class Pupil:
 
-    def __init__ (self):
-        self.name = ""
-        self.year = 0
-        self.marks = []
+    def __init__ (self,in_name,in_age,in_marks):
+        self.name = in_name
+        self.age = in_age
+        self.marks = in_marks
         self.amark = 0
 
     def set_mark(self, current_mark):
@@ -14,3 +14,6 @@ class pupil:
     def average_mark(self):
         return sum(self.marks) / len(self.marks)
           
+    def __str__(self):
+        s = "pupil " + self.name + " aged " + str(self.age) + " has marks " + str(self.marks)
+        return s
